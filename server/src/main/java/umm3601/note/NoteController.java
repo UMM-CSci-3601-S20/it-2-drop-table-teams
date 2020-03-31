d1ea418a88ddeb90abc4710c9f3ab0ebe3865cf1
Joe Walbran <walbr037@morris.umn.edu>
2020-03-31 14:32:40 -0500

    Refactor JWT verification code into its own method
    This method lives in the Server class.

--- server/src/main/java/umm3601/note/NoteController.java
+++ server/src/main/java/umm3601/note/NoteController.java
@@ -4,26 +4,13 @@ import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Filters.regex;
 
-import java.security.PublicKey;
-import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import javax.inject.Inject;
 
-import com.auth0.jwk.InvalidPublicKeyException;
-import com.auth0.jwk.Jwk;
-import com.auth0.jwk.JwkException;
-import com.auth0.jwk.SigningKeyNotFoundException;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.common.collect.ImmutableMap;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -39,9 +26,6 @@ import io.javalin.http.ConflictResponse;
 import io.javalin.http.Context;
 import io.javalin.http.ForbiddenResponse;
 import io.javalin.http.NotFoundResponse;
-import io.javalin.http.UnauthorizedResponse;
-import javalinjwt.JavalinJWT;
-import umm3601.Server;
 import umm3601.UnprocessableResponse;
 
 
@@ -49,7 +33,6 @@ import umm3601.UnprocessableResponse;
  * Controller that manages requests for note data (for a specific owner).
  */
 public class NoteController {
-
 
   private final String ISO_8601_REGEX = "([+-]\\d\\d)?\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d([+, -.])\\d\\d\\d[\\dZ]";
 
@@ -110,55 +93,6 @@ public class NoteController {
    */
   public void deleteNote(Context ctx) {
     String id = ctx.pathParam("id");
-
-    String encodedToken;
-    try {
-      encodedToken = JavalinJWT.getTokenFromHeader(ctx).get();
-    } catch (NoSuchElementException e) {
-      throw new UnauthorizedResponse("Missing token");
-    }
-
-    DecodedJWT decodedToken;
-    try {
-      decodedToken = JWT.decode(encodedToken);
-    } catch (JWTDecodeException e) {
-      throw new UnauthorizedResponse("Malformed token");
-    }
-
-    String keyID = decodedToken.getKeyId();
-
-    if (keyID == null) {
-      throw new UnauthorizedResponse("Token does not contain a key ID");
-    }
-
-    Jwk jsonWebKey;
-    try {
-      jsonWebKey = Server.auth0JwkProvider.get(keyID);
-    } catch (JwkException e) {
-      throw new UnauthorizedResponse(
-        "Token doesn't refer to one of Auth0's public keys");
-    }
-
-    RSAPublicKey publicKey;
-    try {
-      publicKey = (RSAPublicKey)jsonWebKey.getPublicKey();
-    } catch (InvalidPublicKeyException e) {
-      throw new UnauthorizedResponse("Auth0 didn't give us a valid public key");
-    }
-
-    try {
-      Algorithm algorithm = Algorithm.RSA256(publicKey, null);
-      JWTVerifier verifier = JWT.require(algorithm)
-        .withIssuer("auth0")
-        .build();
-
-      // We've already got the decoded token, so we can just ignore verify()'s
-      // return value.
-      // (We're only calling verify() for its side-effects.)
-      verifier.verify(encodedToken);
-    } catch (JWTVerificationException exception) {
-      throw new UnauthorizedResponse("Token provided does not verify");
-    }
 
     String ownerID = ctx.queryParam("ownerid");
     Note note;
