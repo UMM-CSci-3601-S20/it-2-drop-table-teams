84510dca60a6fa54f76fd53392a64c6e687a5db2
Johannes Martinez <mart4695@morris.umn.edu>
2020-04-02 07:14:35 -0500

    Create component.ts file for account page
    The account page will display the owner's email address and name. Addresses #32.

---
+++ client/src/app/account/account.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-account-account.component',
+  templateUrl: 'account.component.html',
+})
+export class AccountComponent {
+}
