ff7458e032f999ebf76097d84d584216217de9c7
Jake <jake.peterson7@aol.com>
2020-04-03 13:24:46 -0500

    Add in encodedTestToken, testKeyID, testPublicKey, and tesPrivateKey into JwtProcessorSpec.
    These will be used for testing JwtProcessor.

--- server/src/test/java/umm3601/note/JwtProcessorSpec.java
+++ server/src/test/java/umm3601/note/JwtProcessorSpec.java
@@ -1,5 +1,85 @@
 package umm3601.note;
 
+import java.io.IOException;
+
+import com.auth0.jwk.JwkProvider;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import umm3601.JwtGetter;
+import umm3601.JwtProcessor;
+
 public class JwtProcessorSpec {
+  @Mock(name = "jwtGetter")
+  public JwtGetter mockJwtGetter;
+
+  @Mock(name = "jwkProvider")
+  public JwkProvider mockJwkProvider;
+
+  @InjectMocks
+  public JwtProcessor jwtProcessor;
+
+  // Generated using jwt.io's debugger tool.
+  public String encodedTestToken =
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZvbyJ9"
+    + ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0"
+    + ".R4ZHIEqY7-c6zVzRD5e3VTxT3mi3agF5wbtXKiJktUk5uQU-x2jJEaEQg_1LPr9n1_4zpzOQecO-AFnk-A9frwmHMdZDMstvLZ4CXQhxe0mveXuIE-BkMNjtK91psuCbks080AKCW-YAsCtB1N5KNFLEbblY9ywAqjXZ5exU7MViRaaUKBXwNJuZx-KURdSdT-VKZ62IiSR2hhg4OjNnbF6_sPMCKoHErrHZHICGNGyjQyqL-HudMi9lTA2Uyktj8i2hlSjhquMgYGGj1lcfKoi1yx9MHwbqKjjrUMwzdRyU68L7TJnwUa5ewhh8X3XP5L8LWoNLVAi7Mt2URqwpkg";
+
+  public String testKeyID = "foo";
+
+  public String testPublicKey =
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnzyis1ZjfNB0bBgKFMSv"
+    + "vkTtwlvBsaJq7S5wA+kzeVOVpVWwkWdVha4s38XM/pa/yr47av7+z3VTmvDRyAHc"
+    + "aT92whREFpLv9cj5lTeJSibyr/Mrm/YtjCZVWgaOYIhwrXwKLqPr/11inWsAkfIy"
+    + "tvHWTxZYEcXLgAXFuUuaS3uF9gEiNQwzGTU1v0FqkqTBr4B8nW3HCN47XUu0t8Y0"
+    + "e+lf4s4OxQawWD79J9/5d3Ry0vbV3Am1FtGJiJvOwRsIfVChDpYStTcHTCMqtvWb"
+    + "V6L11BWkpzGXSW4Hv43qa+GSYOD2QU68Mb59oSk2OB+BtOLpJofmbGEGgvmwyCI9"
+    + "MwIDAQAB";
+
+  // Just for testing--please don't use this in production ;)
+  public String testPrivateKey =
+    "MIIEogIBAAKCAQEAnzyis1ZjfNB0bBgKFMSvvkTtwlvBsaJq7S5wA+kzeVOVpVWw"
+    + "kWdVha4s38XM/pa/yr47av7+z3VTmvDRyAHcaT92whREFpLv9cj5lTeJSibyr/Mr"
+    + "m/YtjCZVWgaOYIhwrXwKLqPr/11inWsAkfIytvHWTxZYEcXLgAXFuUuaS3uF9gEi"
+    + "NQwzGTU1v0FqkqTBr4B8nW3HCN47XUu0t8Y0e+lf4s4OxQawWD79J9/5d3Ry0vbV"
+    + "3Am1FtGJiJvOwRsIfVChDpYStTcHTCMqtvWbV6L11BWkpzGXSW4Hv43qa+GSYOD2"
+    + "QU68Mb59oSk2OB+BtOLpJofmbGEGgvmwyCI9MwIDAQABAoIBACiARq2wkltjtcjs"
+    + "kFvZ7w1JAORHbEufEO1Eu27zOIlqbgyAcAl7q+/1bip4Z/x1IVES84/yTaM8p0go"
+    + "amMhvgry/mS8vNi1BN2SAZEnb/7xSxbflb70bX9RHLJqKnp5GZe2jexw+wyXlwaM"
+    + "+bclUCrh9e1ltH7IvUrRrQnFJfh+is1fRon9Co9Li0GwoN0x0byrrngU8Ak3Y6D9"
+    + "D8GjQA4Elm94ST3izJv8iCOLSDBmzsPsXfcCUZfmTfZ5DbUDMbMxRnSo3nQeoKGC"
+    + "0Lj9FkWcfmLcpGlSXTO+Ww1L7EGq+PT3NtRae1FZPwjddQ1/4V905kyQFLamAA5Y"
+    + "lSpE2wkCgYEAy1OPLQcZt4NQnQzPz2SBJqQN2P5u3vXl+zNVKP8w4eBv0vWuJJF+"
+    + "hkGNnSxXQrTkvDOIUddSKOzHHgSg4nY6K02ecyT0PPm/UZvtRpWrnBjcEVtHEJNp"
+    + "bU9pLD5iZ0J9sbzPU/LxPmuAP2Bs8JmTn6aFRspFrP7W0s1Nmk2jsm0CgYEAyH0X"
+    + "+jpoqxj4efZfkUrg5GbSEhf+dZglf0tTOA5bVg8IYwtmNk/pniLG/zI7c+GlTc9B"
+    + "BwfMr59EzBq/eFMI7+LgXaVUsM/sS4Ry+yeK6SJx/otIMWtDfqxsLD8CPMCRvecC"
+    + "2Pip4uSgrl0MOebl9XKp57GoaUWRWRHqwV4Y6h8CgYAZhI4mh4qZtnhKjY4TKDjx"
+    + "QYufXSdLAi9v3FxmvchDwOgn4L+PRVdMwDNms2bsL0m5uPn104EzM6w1vzz1zwKz"
+    + "5pTpPI0OjgWN13Tq8+PKvm/4Ga2MjgOgPWQkslulO/oMcXbPwWC3hcRdr9tcQtn9"
+    + "Imf9n2spL/6EDFId+Hp/7QKBgAqlWdiXsWckdE1Fn91/NGHsc8syKvjjk1onDcw0"
+    + "NvVi5vcba9oGdElJX3e9mxqUKMrw7msJJv1MX8LWyMQC5L6YNYHDfbPF1q5L4i8j"
+    + "8mRex97UVokJQRRA452V2vCO6S5ETgpnad36de3MUxHgCOX3qL382Qx9/THVmbma"
+    + "3YfRAoGAUxL/Eu5yvMK8SAt/dJK6FedngcM3JEFNplmtLYVLWhkIlNRGDwkg3I5K"
+    + "y18Ae9n7dHVueyslrb6weq7dTkYDi3iOYRW8HRkIQh06wEdbxt0shTzAJvvCQfrB"
+    + "jg/3747WSsf/zBTcHihTRBdAv6OmdhV4/dD5YBfLAkLrd+mX7iE=";
+
+  @BeforeEach
+  public void setupEach() throws IOException {
+    MockitoAnnotations.initMocks(this);
+  }
 
 }
