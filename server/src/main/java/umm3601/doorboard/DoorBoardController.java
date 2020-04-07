879e666c10b9b989ce84fe11adada48feea62197
Joe Walbran <walbr037@morris.umn.edu>
2020-04-07 18:18:38 -0500

    Implement getDoorBoards
    This causes two tests to pass.
    
    Addresses #36.

--- server/src/main/java/umm3601/doorboard/DoorBoardController.java
+++ server/src/main/java/umm3601/doorboard/DoorBoardController.java
@@ -3,25 +3,39 @@ package umm3601.doorboard;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
+import org.mongojack.JacksonCodecRegistry;
 
 import io.javalin.http.Context;
 import umm3601.JwtProcessor;
 
 public class DoorBoardController {
-  private MongoDatabase database;
+  private MongoCollection<DoorBoard> doorBoards;
   private JwtProcessor jwtProcessor;
 
   public DoorBoardController(
-    MongoDatabase database,
-    JwtProcessor jwtProcessor) {
-      this.database = database;
-      this.jwtProcessor = jwtProcessor;
+      MongoDatabase database,
+      JwtProcessor jwtProcessor) {
+    JacksonCodecRegistry codecs =
+        JacksonCodecRegistry.withDefaultObjectMapper();
+    codecs.addCodecForClass(DoorBoard.class);
+
+    this.doorBoards = database.getCollection("doorBoards")
+        .withDocumentClass(DoorBoard.class)
+        .withCodecRegistry(codecs);
+
+    this.jwtProcessor = jwtProcessor;
   }
 
   public void getDoorBoards(Context ctx) {
+    List<DoorBoard> doorBoardList = doorBoards
+      .find(new Document())
+      .into(new ArrayList<>());
+
+    ctx.json(doorBoardList);
   }
 
   public void getDoorBoard(Context ctx) {
