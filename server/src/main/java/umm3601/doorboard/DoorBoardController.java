9bdd151fa14d8dc3a955bde0df62cd57f5d9876c
Joe Walbran <walbr037@morris.umn.edu>
2020-04-07 18:30:10 -0500

    Implement getDoorBoard
    This makes four tests pass.
    
    Addresses #36.

--- server/src/main/java/umm3601/doorboard/DoorBoardController.java
+++ server/src/main/java/umm3601/doorboard/DoorBoardController.java
@@ -7,9 +7,12 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.mongojack.JacksonCodecRegistry;
 
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 import umm3601.JwtProcessor;
 
 public class DoorBoardController {
@@ -39,6 +42,27 @@ public class DoorBoardController {
   }
 
   public void getDoorBoard(Context ctx) {
+    ObjectId doorBoardId;
+    try {
+      // The path parameter should always be present;
+      // there should be no danger of a NullPointerException
+      // here.
+      doorBoardId = new ObjectId(ctx.pathParam("id"));
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse(
+        ctx.pathParam("id") + " is not a well-formed id");
+    }
+
+    DoorBoard doorBoard = doorBoards
+        .find(new Document().append("_id", doorBoardId))
+        .first();
+
+    if (doorBoard == null) {
+      throw new NotFoundResponse(
+        ctx.pathParam("id") + " does not match any DoorBoard");
+    }
+
+    ctx.json(doorBoard);
   }
 
   public void addNewDoorBoard(Context ctx) {
