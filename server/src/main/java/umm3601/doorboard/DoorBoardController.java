55e2761764317dcb1818fa18198579283589341b
Joe Walbran <walbr037@morris.umn.edu>
2020-04-07 17:47:14 -0500

    Add stubs for methods in DoorBoardController
    This makes the tests compile, but not pass.

--- server/src/main/java/umm3601/doorboard/DoorBoardController.java
+++ server/src/main/java/umm3601/doorboard/DoorBoardController.java
@@ -1,13 +1,32 @@
 package umm3601.doorboard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mongodb.client.MongoDatabase;
 
+import org.bson.Document;
+
+import io.javalin.http.Context;
 import umm3601.JwtProcessor;
 
 public class DoorBoardController {
+  private MongoDatabase database;
+  private JwtProcessor jwtProcessor;
+
   public DoorBoardController(
     MongoDatabase database,
     JwtProcessor jwtProcessor) {
-      // TODO
+      this.database = database;
+      this.jwtProcessor = jwtProcessor;
+  }
+
+  public void getDoorBoards(Context ctx) {
+  }
+
+  public void getDoorBoard(Context ctx) {
+  }
+
+  public void addNewDoorBoard(Context ctx) {
   }
 }
