071f71cbf1eb54e2986ba4b8d50644e6785a5808
Joe Walbran <walbr037@morris.umn.edu>
2020-04-07 18:45:03 -0500

    Implement addNewDoorBoard
    This causes four tests to pass.
    
    Addresses #36.

--- server/src/main/java/umm3601/doorboard/DoorBoardController.java
+++ server/src/main/java/umm3601/doorboard/DoorBoardController.java
@@ -3,6 +3,8 @@ package umm3601.doorboard;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.common.collect.ImmutableMap;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -12,6 +14,7 @@ import org.mongojack.JacksonCodecRegistry;
 
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.http.ForbiddenResponse;
 import io.javalin.http.NotFoundResponse;
 import umm3601.JwtProcessor;
 
@@ -66,5 +69,24 @@ public class DoorBoardController {
   }
 
   public void addNewDoorBoard(Context ctx) {
+    // This will throw an UnauthorizedException if the user isn't logged in.
+    DecodedJWT jwt = jwtProcessor.verifyJwtFromHeader(ctx);
+
+    DoorBoard doorBoard = ctx.bodyValidator(DoorBoard.class)
+        .check(body -> body.name != null && !body.name.isEmpty())
+        .check(body -> body.building != null && !body.building.isEmpty())
+        .check(body -> body.officeNumber != null && !body.officeNumber.isEmpty())
+        .check(body -> body.email != null && !body.email.isEmpty())
+        .check(body -> body.email.contains("@"))
+        .get();
+
+    if (!doorBoard.sub.equals(jwt.getSubject())) {
+      throw new ForbiddenResponse("Can't make a DoorBoard for another user");
+    }
+
+    doorBoards.insertOne(doorBoard);
+
+    ctx.status(201);
+    ctx.json(ImmutableMap.of("id", doorBoard._id));
   }
 }
