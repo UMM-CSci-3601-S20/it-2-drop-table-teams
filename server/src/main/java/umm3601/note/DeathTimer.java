bdbda8b54ace5ecb6ae164209a35271b13993fd7
Jake <jake.peterson7@aol.com>
2020-04-07 14:29:57 -0500

    Fix spurious javadoc @param tag.
    The colon got sucked into the param name, it was "n:" now it is "n".

--- server/src/main/java/umm3601/note/DeathTimer.java
+++ server/src/main/java/umm3601/note/DeathTimer.java
@@ -47,7 +47,7 @@ public class DeathTimer extends Timer {
    * set before they are permanently purged from the database. This timer is
    * currently set to one week.
    *
-   * @param n: The note whose timer will be updated.
+   * @param n The note whose timer will be updated.
    * @return true if the note now has a timer attached to it, false otherwise.
    */
   public boolean updateTimerStatus(Note n) {
