6370fdc7def39708891d1b9287beb28339902439
Joe Walbran <walbr037@morris.umn.edu>
2020-04-09 01:40:25 -0500

    Add more tests for InterceptorService
    They ended up being somewhat involved. This is partially because we have a lot of test data and partially because RxJS is hard.
    
    Addresses #14.

--- client/src/app/auth/interceptor.service.spec.ts
+++ client/src/app/auth/interceptor.service.spec.ts
@@ -1,73 +1,110 @@
 import { HttpHandler, HttpRequest, HttpParams } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { InterceptorService } from './interceptor.service';
 import { AuthService } from './auth.service';
+import { of } from 'rxjs';
 
 describe('The interceptor', () => {
   let interceptor: InterceptorService;
   let auth: AuthService;
   let next: HttpHandler;
-  let requestsAndWhetherTheyreSecure: [HttpRequest<any>, boolean][];
+
+  const requestsAndWhetherTheyreSecure: [HttpRequest<any>, boolean][] = [
+    [
+      new HttpRequest<any>('GET', '/api/notes'),
+      true,
+    ],
+    [
+      new HttpRequest<any>('GET', '/api/notes', {
+        params: new HttpParams().append('status', 'active')}
+      ),
+      false,
+    ],
+    [
+      new HttpRequest<any>('GET', '/api/notes', {
+        params: new HttpParams().append('status', 'draft')}
+      ),
+      true,
+    ],
+    [
+      new HttpRequest<any>('POST', '/api/notes/new', { iAm: 'the request body' }),
+      true,
+    ],
+    [
+      new HttpRequest<any>('DELETE', '/api/notes/12121212'),
+      true,
+    ],
+    [
+      new HttpRequest<any>('PATCH', '/api/notes/12121212', { iAm: 'the request body' }),
+      true,
+    ],
+    [
+      new HttpRequest<any>('GET', '/api/doorBoards'),
+      false,
+    ],
+    [
+      new HttpRequest<any>('GET', '/api/doorBoards/12121212'),
+      false,
+    ],
+    [
+      new HttpRequest<any>('POST', '/api/doorBoards/new', { iAm: 'the request body' }),
+      true,
+    ]
+  ];
 
   beforeEach(() => {
-    // Set up the mock handling of the HTTP requests
     TestBed.configureTestingModule({});
 
-    auth = jasmine.createSpyObj('auth', ['getTokenSilently$']);
+    auth = jasmine.createSpyObj('auth', ['getTokenSilently$']) as jasmine.SpyObj<AuthService>;
+    (auth as jasmine.SpyObj<AuthService>)
+      .getTokenSilently$.and.callFake(() => of('super secure token'));
 
     interceptor = new InterceptorService(auth);
 
     next = jasmine.createSpyObj('next', ['handle']);
-
-    requestsAndWhetherTheyreSecure = [
-      [
-        new HttpRequest<any>('GET', '/api/notes'),
-        true,
-      ],
-      [
-        new HttpRequest<any>('GET', '/api/notes', {
-          params: new HttpParams().append('status', 'active')}
-        ),
-        false,
-      ],
-      [
-        new HttpRequest<any>('GET', '/api/notes', {
-          params: new HttpParams().append('status', 'draft')}
-        ),
-        true,
-      ],
-      [
-        new HttpRequest<any>('POST', '/api/notes/new', { iAm: 'the request body' }),
-        true,
-      ],
-      [
-        new HttpRequest<any>('DELETE', '/api/notes/12121212'),
-        true,
-      ],
-      [
-        new HttpRequest<any>('PATCH', '/api/notes/12121212', { iAm: 'the request body' }),
-        true,
-      ],
-      [
-        new HttpRequest<any>('GET', '/api/doorBoards'),
-        false,
-      ],
-      [
-        new HttpRequest<any>('GET', '/api/doorBoards/12121212'),
-        false,
-      ],
-      [
-        new HttpRequest<any>('POST', '/api/doorBoards/new', { iAm: 'the request body' }),
-        true,
-      ]
-    ];
-
+    // next.handle(...) needs to return an observable or else RxJS will
+    // complain!
+    (next as jasmine.SpyObj<HttpHandler>)
+      .handle.and.callFake(() => of(null));
   });
 
   describe('The shouldThereBeAToken() method', () => {
-    it('correctly determines whether or not there should be a JWT attached to any given request', () => {
+    describe('correctly determines whether or not there should be a JWT attached to the following requests:', () => {
+      console.log(requestsAndWhetherTheyreSecure);
       for (const [request, isItSecure] of requestsAndWhetherTheyreSecure) {
-        expect(interceptor.shouldThereBeAToken(request)).toBe(isItSecure);
+        it(`${request.method} ${request.url}`, () => {
+          expect(interceptor.shouldThereBeAToken(request)).toBe(isItSecure);
+        });
+      }
+    });
+  });
+
+  describe('The intecept() method', () => {
+    describe('calls next.handle(...) when given the following requests:', () => {
+      for (const [request] of requestsAndWhetherTheyreSecure) {
+        it(`${request.method} ${request.url}`, async(() => {
+          interceptor.intercept(request, next).subscribe(() => {
+            expect(next.handle).toHaveBeenCalledTimes(1);
+          });
+        }));
+      }
+    });
+
+    describe('puts a JWT in the header in a given request iff that request is secure:', () => {
+      for (const [request, isItSecure] of requestsAndWhetherTheyreSecure) {
+        it(`${request.method} ${request.url}`, async(() => {
+          interceptor.intercept(request, next).subscribe(() => {
+            const theRequestPassedAlong = (next as jasmine.SpyObj<HttpHandler>)
+              .handle.calls.argsFor(0)[0];
+
+            expect(theRequestPassedAlong.headers.has('Authorization')).toBe(isItSecure);
+
+            if (isItSecure) {
+              expect(theRequestPassedAlong.headers.get('Authorization'))
+                .toBe('Bearer super secure token');
+            }
+          });
+        }));
       }
     });
   });
