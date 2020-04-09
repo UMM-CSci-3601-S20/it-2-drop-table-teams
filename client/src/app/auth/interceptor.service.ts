910ef485882e9079c7ddb13f7938fd577d6cf7e2
Joe Walbran <walbr037@morris.umn.edu>
2020-04-08 23:11:32 -0500

    Add logic to decide which routes should be intercepted
    This logic is untested at the moment.
    
    Addresses #14.

--- client/src/app/auth/interceptor.service.ts
+++ client/src/app/auth/interceptor.service.ts
@@ -18,10 +18,49 @@ export class InterceptorService implements HttpInterceptor {
 
   constructor(private auth: AuthService) { }
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.shouldThereBeAToken(req)) {
+      return this.requestWithTokenAttached(req, next);
+    }
+
+    return next.handle(req);
+  }
+
+  private shouldThereBeAToken(req: HttpRequest<any>): boolean {
+    let endpoint: string;
+    const match = req.url.match(/^.*\/api\/(.*?)\/?$/);
+    if (match) {
+      endpoint = match.groups[1];
+    } else {
+      // This request isn't going to our api; don't bother
+      // including a JWT.
+      return false;
+    }
+
+    switch (req.method) {
+      case 'GET':
+        if (endpoint.startsWith('doorBoards')) {
+          // DoorBoards are always public.
+          return false;
+        } else if (endpoint.startsWith('notes')) {
+          // Active notes are public; all other notes are private.
+          if (req.params.get('status') === 'active') {
+            return false;
+          } else {
+            return true;
+          }
+        } else {
+          // Assume that all other GET requests are public.
+          return false;
+        }
+
+      default:
+        // Assume that all other requests are private.
+        return true;
+    }
+  }
+
+  private requestWithTokenAttached(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return this.auth.getTokenSilently$().pipe(
       mergeMap(token => {
         const tokenReq = req.clone({
