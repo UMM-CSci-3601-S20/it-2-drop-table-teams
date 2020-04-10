e3fe12ea4cf7ec240e12bc6ff3882a8950fb2a52
Johannes Martinez <mart4695@morris.umn.edu>
2020-04-10 04:21:53 -0500

    Replace double quotes with single quotes in edit-note.component.ts

--- client/src/app/notes/edit-note.component.ts
+++ client/src/app/notes/edit-note.component.ts
@@ -9,7 +9,7 @@ import { OnInit, Component } from '@angular/core';
 
 export class EditNoteComponent implements OnInit {
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
   constructor() {}
